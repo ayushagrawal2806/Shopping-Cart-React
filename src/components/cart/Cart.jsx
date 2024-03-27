a2928abcb0c843005602b131cc94918415635637
ayushagrawal <ayushagrawal6082@gmail.com>
2024-03-27 21:18:36 +0530

    Formated

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,26 +5,27 @@ import cartContext from "../../context/context";
 const Cart = () => {
   const { product, price } = useContext(cartContext);
   const arr = [];
-  product.filter((element) => (element.quantity > 0) ? arr.push(element) : "" )
+  product.filter((element) => (element.quantity > 0 ? arr.push(element) : ""));
   return (
     <div className="cart">
       <h1>Cart</h1>
       <div className="cartProducts">
-        {
-          (!arr.length) ? <p className="noAdded">No Product Added</p>
-          : 
+        {!arr.length ? (
+          <p className="noAdded">No Product Added</p>
+        ) : (
           arr.map((element) => {
-            
-              return (
-                <div className="cartProduct" key={element.id}>
-                  <p>{element.name}</p>
-                  <div className="priceQuantity">
-                    <p>{element.quantity} × {element.price}</p>
-                  </div>
-                 </div> 
-              )
+            return (
+              <div className="cartProduct" key={element.id}>
+                <p>{element.name}</p>
+                <div className="priceQuantity">
+                  <p>
+                    {element.quantity} × {element.price}
+                  </p>
+                </div>
+              </div>
+            );
           })
-        }
+        )}
       </div>
       <div className="totalPrice">
         <h2>Total : </h2>
@@ -35,7 +36,3 @@ const Cart = () => {
 };
 
 export default Cart;
-
-
-
-
